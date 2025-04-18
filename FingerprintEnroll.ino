f7d09bf5751fa967bab5f7389c49b3bec03c718c
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-04-18 11:53:44 +0530

    c3

--- FingerprintEnroll.ino
+++ FingerprintEnroll.ino
@@ -1,72 +1,37 @@
 #include <Adafruit_Fingerprint.h>
-#include <SoftwareSerial.h>
 
-#define RX_PIN 2
-#define TX_PIN 3
-#define SOLENOID_PIN 4
-
-SoftwareSerial mySerial(RX_PIN, TX_PIN);
-
+SoftwareSerial mySerial(2, 3);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
-void setup() {
+uint8_t id;
+
+void setup()
+{
   Serial.begin(9600);
-  mySerial.begin(57600);
+  while (!Serial);  
+  delay(100);
 
-  pinMode(SOLENOID_PIN, OUTPUT);
-  digitalWrite(SOLENOID_PIN, LOW);
-
-  if (!finger.begin()) {
-    Serial.println("Couldn't find fingerprint sensor :(");
-    while (1);
-  }
-
-  if (finger.verifyPassword()) {
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  if (finger.verifyPassword()) 
+  {
     Serial.println("Found fingerprint sensor!");
-  } else {
+  } 
+  else 
+  {
     Serial.println("Did not find fingerprint sensor :(");
-    while (1);
-  }
-}
-
-void loop() {
-  Serial.println("Place your finger on the sensor.");
-
-  if (finger.getImage() == FINGERPRINT_OK) {
-    Serial.println("Image taken");
-    delay(500);
-
-    int id = -1;
-
-    while (id == -1) {
-      id = finger.getImageFingerID();
-      delay(50);
-    }
-
-    Serial.print("Finger ID found: "); Serial.println(id);
-
-    if (finger.createModel() == FINGERPRINT_OK) {
-      Serial.println("Remove your finger");
-      delay(2000);
-      Serial.println("Place the same finger again");
-
-      while (finger.getImage() != FINGERPRINT_OK);
-
-      if (finger.createModel() == FINGERPRINT_OK) {
-        Serial.println("Fingerprint enrolled successfully!");
-
-        // Activate the solenoid lock
-        digitalWrite(SOLENOID_PIN, HIGH);
-        delay(5000); // Keep the solenoid active for 5 seconds (you can adjust as needed)
-        digitalWrite(SOLENOID_PIN, LOW);
-
-      } else {
-        Serial.println("Failed to enroll fingerprint, please try again.");
-      }
-    } else {
-      Serial.println("Failed to create model, please try again.");
-    }
+    while (1) { delay(1); }
   }
 
-  delay(1000); // Wait for a second before the next attempt
+  Serial.println(F("Reading sensor parameters"));
+  finger.getParameters();
+  finger.getTemplateCount();
+  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
+  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
+  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
+  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
+  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
+  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
+  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
+  Serial.print(F("Total finger prints stored: ")); Serial.println(finger.templateCount);
 }
