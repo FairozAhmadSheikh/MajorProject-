91e38a13d97d756825a2d43120c24ac2701dc265
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-04-18 11:50:26 +0530

    c2

--- CodeForLockControl.ino
+++ CodeForLockControl.ino
@@ -23,3 +23,29 @@ void setup()
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH);   //Switch off relay initially. Relay is LOW level triggered relay so we need to write HIGH.
 }
+void loop()
+{
+  if ( getFingerPrint() != -1)
+  {
+    digitalWrite(RELAY_PIN, LOW);
+    delay(ACCESS_DELAY);
+    digitalWrite(RELAY_PIN, HIGH);   
+  }  
+  delay(50);            //Add some delay before next scan.
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerPrint() 
+{
+  int p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  // found a match!
+  return finger.fingerID;
+}
